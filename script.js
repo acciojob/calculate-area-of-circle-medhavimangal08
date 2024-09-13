897b2bda7cc93a062aca88b8c00dee9f7d9258ff
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 17:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
  function calculateArea() {
+	 
             // Get the radius from the user using the prompt() function
     let radius = prompt("Enter the radius of the circle:");
-	 let area=Math.PI*radius*radius
-	 alert(`The area of the circle with radius ${radius} is ${area.toFixed(2)})
+	let area=Math.PI*radius*radius;
+	 alert(`The area of the circle with radius ${radius} is ${area.toFixed(2)}`)
 	 
 	 
 	 
